ed3636fd71cab85cda6e21981f79b201ca17557b
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2024-04-04 14:11:16 +0900

    0403

---
+++ 5week/app_backup.js
@@ -0,0 +1,61 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/",(req,res)=>{
+//     res.status(200);
+//     res.send("hello");
+// });
+// app.listen(port,()=>{
+//     console.log(`${port}번 포트에서 서버 실행중`);
+// });
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/",(req,res)=>{
+//     res.status(200);
+//     res.send("fuck you");
+// });
+
+// app.get("/contact",(req,res)=>{
+//     res.status(200).send("contact page");
+// });
+
+// app.listen(port, ()=>{
+//     console.log(`${port}번 포트에서 서버 실행 중`);
+// });
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.status(200);
+    res.send("Hello Node!");
+});
+// 모든 연락처 가져오기
+app.get("/contacts",(req,res)=>{
+    res.status(200).send("Contacts Page");
+});
+// 새 연락처 추가하기
+app.post("/contacts",(req, res)=>{
+    res.status(201).send("Create Contacts");
+});
+// 연락처 상세보기
+app.get("/contacts/:id",(req, res)=>{
+    res.status(200).send(`View Contact for ID :${req.params.id}`);
+});
+// 연락처 수정하기
+app.put("/contacts/:id",(req, res)=>{
+    res.status(200).send(`Update Contact for ID :${req.params.id}`);
+});
+// 연락처 삭제하기
+app.delete("/contacts/:id",(req, res)=>{
+    res.status(200).send(`Delete Contact for ID :${req.params.id}`);
+});
+
+app.listen(port,()=>{
+    console.log(`${port}번 포트에서 서버 실행 중`);
+});
