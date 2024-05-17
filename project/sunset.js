7cc6f5caafc78e7c0b125beb8ef6d6061c5c6007
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2024-05-17 10:10:32 +0900

    1010

--- project/sunset.js
+++ project/sunset.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db")
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+connectDb();
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
@@ -11,7 +16,16 @@ app.set("views", "./views")
 
 app.use(express.static("public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(methodOverride("_method"));
+
+app.use(cookieParser());
+
 app.use("/", require("./routes/main"));
+app.use("/", require("./routes/admin"));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
